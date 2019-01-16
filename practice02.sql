07df3f73c3bd145fdeb07f2af9441d607ec68ae4
woohyun <woohyun@LAPTOP-79URTKQS>
2019-01-16 23:16:24 +0900

    practice01-3번보충 practice02-6번확인

--- practice02.sql
+++ practice02.sql
@@ -2,19 +2,19 @@ select * from employees;
 select * from salaries;
 
 -- 1번
-select (max(salary)-min(salary)) as '최고임금 - 최저임금' from salaries ;
+select max(salary) as '최고임금', min(salary) as '최저임금',(max(salary)-min(salary)) as '최고임금 - 최저임금' from salaries ;
 			
--- 2번
-select date_format(max(from_date),'%Y년 %m월 %d일') from salaries order by from_Date;
+-- 2번 -- hire_date
+select date_format(max(hire_date),'%Y년 %m월 %d일') from employees;
 
 -- 3번
-select date_format(min(from_date),'%Y년 %m월 %d일') from salaries;
+select date_format(min(hire_date),'%Y년 %m월 %d일') from employees;
 
 -- 4번
-select avg(salary) from salaries; 
+select avg(salary) from salaries where to_date like '9999-01-01'; 
 
 -- 5번
-select max(salary),min(salary) from salaries;
+select max(salary),min(salary) from salaries where to_date like '9999-01-01';
 
--- 6번
+-- 6번 -- 보충
 select (2019-max(from_date)), (2019-min(from_date)) from salaries;
